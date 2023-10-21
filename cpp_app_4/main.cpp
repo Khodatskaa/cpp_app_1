4fef6fe6533c0d166d1b81ad190be5f1aa7c1a9d
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 19:24:39 +0300

    Ex 3 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main()
 {
+	double V;
+	double t;
+	double a;
+	double S;
+
+	std::cout << "Enter speed (V): ";
+	std::cin >> V;
+
+	std::cout << "Enter time (t): ";
+	std::cin >> t;
+
+	std::cout << "Enter acceleration (a): ";
+	std::cin >> a;
+
+	S = V * t + (a * t * t) / 2;
+
+	std::cout << "Distance walked (S) = " << S << std::endl;
+
 	return 0;
 }
