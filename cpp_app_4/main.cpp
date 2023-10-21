85952dd7d3ed9f13c2b40bbd8676d581e35fcfeb
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 22:47:57 +0300

    Ex 3 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main()
-{
-	return 0;
+int main() {
+    double length, width, height;
+
+    std::cout << "Calculation of pralelepiped volume" << std::endl;
+
+    std::cout << "Enter the length of parallelepiped: ";
+    std::cin >> length;
+
+    std::cout << "Enter the width of parallelepiped: ";
+    std::cin >> width;
+
+    std::cout << "Enter the height of parallelepiped: ";
+    std::cin >> height;
+
+    double volume = length * width * height;
+
+    std::cout << "Volume of parallelepiped: " << volume << " cubic cm" << std::endl;
+
+    return 0;
 }
