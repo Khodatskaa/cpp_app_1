76460cbff831e2e97d269462f5c5afaa7d217834
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 23:03:36 +0300

    Ex 5 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-	return 0;
+const double PI = 3.14159265358979323846;
+
+
+int main() {
+    double radius;
+
+    std::cout << "Enter the radius of the sphere: ";
+    std::cin >> radius;
+
+    double volume = (radius > 0) ? (4.0 / 3.0) * PI * std::pow(radius, 3) : 0;
+
+    std::cout << "Volume of the sphere: " << volume << " cubic cm" << std::endl;
+
+    return 0;
 }
