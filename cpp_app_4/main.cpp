ba69228ce20aaff41d0f9852c74b72e155a8a971
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 19:22:25 +0300

    Ex 2 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 int main()
 {
+	double L;
+	double R;
+	double S;
+
+	std::cout << "Enter the length of the circle: ";
+	std::cin >> L;
+
+	R = L / (2 * M_PI);
+
+	S = M_PI * pow(R, 2);
+
+	std::cout << "Circle radius (R) = " << R << std::endl;
+	std::cout << "Circle area (S) = " << S << std::endl;
+
 	return 0;
 }
