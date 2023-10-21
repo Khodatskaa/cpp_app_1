4d614d45cf11beae5d469f6550e8708f826f0bec
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 22:10:55 +0300

    Ex 1 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
+    int year;
+    std::cout << "Enter the year: ";
+    std::cin >> year;
+
+    int isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    int days = 365 + (isLeapYear ? 1 : 0);
+
+    std::cout << "In " << year << " year = " << days << " days" << std::endl;
+
 	return 0;
 }
