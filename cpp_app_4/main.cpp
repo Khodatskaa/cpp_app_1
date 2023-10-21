2b66c8678cf0402a1e31a0e326075c1bcc1d7a87
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 22:19:40 +0300

    Ex 2 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int hrn, kop;
+
+    std::cout << "Enter the amount of hryvnia: ";
+    std::cin >> hrn;
+
+    std::cout << "Enter the amount of kop: ";
+    std::cin >> kop;
+
+    hrn += kop / 100;
+    kop = kop % 100;
+
+    std::cout  << hrn << " hrn " << kop << " kop " << std::endl;
+
+
 	return 0;
 }
