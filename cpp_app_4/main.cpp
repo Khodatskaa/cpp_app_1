9881ff42db0443a519b85f1e9b5db885d41cb453
Khodatskaa <khodatskaalina@gmail.com>
2023-10-21 22:55:19 +0300

    Ex 4 was done

--- cpp_app_4/main.cpp
+++ cpp_app_4/main.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+	std::cout << "Calculation of distance between settlements" << std::endl;
+	std::cout << "Enter output: " << std::endl;
+
+	double mapScale, distanceInCm;
+
+	std::cout << "Enter the scale of the map (the number of kilometers in one centimeter) -> ";
+	std::cin >> mapScale;
+
+	std::cout << "Enter the distance between points representing settlements (cm) -> ";
+	std::cin >> distanceInCm;
+
+	double distanceInKm = distanceInCm / (100000 * mapScale); 
+
+	std::cout << "Distance between settlements " << distanceInKm << "km" << std::endl;
+
+
 	return 0;
 }
